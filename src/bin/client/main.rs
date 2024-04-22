8d453c0a6a542b5deba629934936db94c65e22ff
Jon Klein <jonklein2021@gmail.com>
2024-04-22 19:11:36 -0400

    migrate client to tokio, clean up both source files

--- src/bin/client/main.rs
+++ src/bin/client/main.rs
@@ -1,22 +1,44 @@
-use std::net::TcpStream;
-use std::io::{Read, Write};
+use std::io;
+use std::error::Error;
+use tokio::net::TcpStream;
 
-fn main() {
-    let mut stream = match TcpStream::connect("127.0.0.1:8080") {
-        Ok(s) => s,
-        Err(e) => {
-            println!("Couldn't connect to server: {}", e);
-            return;
-        }
-    };
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let addr = "127.0.0.1:8080";
+    let stream = TcpStream::connect(addr).await?;
 
-    let mut response_buffer = [0; 1024];
+    // wait for the socket to be writable
+    stream.writable().await?;
     
-    // write message to server
-    stream.write("Hello server!".as_bytes()).expect("Failed to write response to server :(");
+    // loop until write to server is successful
+    loop {
+        match stream.try_write(b"Hello Server!") {
+            Ok(_) => break,
+            Err(ref e) if e.kind() == io::ErrorKind::WouldBlock => continue,
+            Err(e) => return Err(e.into()),
+        }
+    }    
+
+    // wait until server is readable
+    stream.readable().await?;
+    
+    let mut response_buffer = vec![0; 1024];
+
+    // loop until stream is read into buffer successfully
+    loop {
+        match stream.try_read(&mut response_buffer) {
+            Ok(n) => {
+                response_buffer.truncate(n);
+                break;
+            },
+            Err(ref e) if e.kind() == io::ErrorKind::WouldBlock => continue, // blocking error; try again
+            Err(e) => return Err(e.into()), // panic if any other other error
+        }
+    }
     
     // read server response
-    stream.read(&mut response_buffer).expect("Failed to read response from server :(");
     let stringified_response = String::from_utf8_lossy(&mut response_buffer);
     println!("From server: {}", stringified_response);
+
+    Ok(())
 }
--- src/bin/server/main.rs
+++ src/bin/server/main.rs
@@ -1,8 +1,9 @@
 use std::io;
 use std::error::Error;
+use std::net::SocketAddr;
 use tokio::net::{TcpListener, TcpStream};
 
-async fn handle_client(stream: TcpStream) -> Result<(), Box<dyn Error>> {
+async fn handle_client(stream: TcpStream, addr: SocketAddr) -> Result<(), Box<dyn Error>> {
     let mut request_buffer = vec![0; 1024];
 
     // wait until client is readable
@@ -14,19 +15,15 @@ async fn handle_client(stream: TcpStream) -> Result<(), Box<dyn Error>> {
             Ok(n) => {
                 request_buffer.truncate(n);
                 break;
-            }
-            Err(ref e) if e.kind() == io::ErrorKind::WouldBlock => {
-                continue; // blocking error; try again
-            }
-            Err(e) => {
-                return Err(e.into()); // panic if any other other error
-            } 
+            },
+            Err(ref e) if e.kind() == io::ErrorKind::WouldBlock => continue, // blocking error; try again
+            Err(e) => return Err(e.into()), // panic if any other other error            } 
         }
     }
 
     // print client message
-    let request = String::from_utf8_lossy(&request_buffer);
-    println!("From client: {}", request);
+    let stringified_request = String::from_utf8_lossy(&request_buffer);
+    println!("From client on {}: {}", addr.ip(), stringified_request);
 
     // wait for the socket to be writable
     stream.writable().await?;
@@ -34,15 +31,9 @@ async fn handle_client(stream: TcpStream) -> Result<(), Box<dyn Error>> {
     // loop until write is successful
     loop {
         match stream.try_write(b"Hello Client!") {
-            Ok(_n) => {
-                break;
-            }
-            Err(ref e) if e.kind() == io::ErrorKind::WouldBlock => {
-                continue; // readiness event is a false positive
-            }
-            Err(e) => {
-                return Err(e.into());
-            }
+            Ok(_) => break,
+            Err(ref e) if e.kind() == io::ErrorKind::WouldBlock => continue, // readiness event is a false positive
+            Err(e) => return Err(e.into()),
         }
     }
 
@@ -52,18 +43,18 @@ async fn handle_client(stream: TcpStream) -> Result<(), Box<dyn Error>> {
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     let addr = "127.0.0.1:8080";
-    let listener = TcpListener::bind(addr).await.unwrap();
+    let listener = TcpListener::bind(addr).await?;
     println!("Server listening on {}", addr);
 
+    // handle client connections in infinite loop
     loop {
         match listener.accept().await {
-            Ok((socket, _)) => { // socket is tokio::net::TcpStream, _ is address
-                if let Err(e) = handle_client(socket).await {
+            Ok((socket, addr)) => { // socket is tokio::net::TcpStream, _ is address
+                if let Err(e) = handle_client(socket, addr).await {
                     return Err(e.into());
                 }
             }
             Err(e) => return Err(e.into())
         }
     }
-    
 }
